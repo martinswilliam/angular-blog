392ac8fed0a3e063eec18b292717f01a2864179a
martinswilliam <williamgmca@gmail.com>
2023-09-30 18:05:43 -0300

    create data fake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+{
+  "id":1,
+  "photo":"aaa",
+  "title":"Saiu novo Episodio",
+  "description": "Olá"
+},
+
+{
+  "id":2,
+  "photo":"aaa",
+  "title": "Saiu novo Episodio",
+  "description": "Olá"
+}
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  photoCover: string = "https://i.pinimg.com/originals/02/24/c8/0224c822ea6ce2f4e6d35ad000cc3300.jpg"
+  contentTitle: string = "Minha pagina"
+  contentDescription: string = "Olá mundo"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
